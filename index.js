5954c24f391cb421c4a5d9be7d72b29368141bed
Deniz Dogan <dogan.deniz.cs@gmail.com>
2024-05-11 04:27:22 +0300

    from 8 to 10 Solutions Added

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ var routeRequest = (name) => {
         case sideBarElementNameList[6]:
             answerTag.innerHTML = name + " : " + getNThPrimeNumber(10001);
             break;
+        case sideBarElementNameList[7]:
+            answerTag.innerHTML = name + " : " + getMaxProductWithGivenDigit(13);
+            break;
+        case sideBarElementNameList[8]:
+            answerTag.innerHTML = name + " : " + findSpecialPythagoreanTriplet(1000);
+            break;
+        case sideBarElementNameList[9]:
+            answerTag.innerHTML = name + " : " + summationOfPrimes(2000000);
+            break;
         default:
             break;
     }
@@ -255,3 +264,76 @@ var getNThPrimeNumber = (n) => {
     }
     return num;
 }
+
+/**
+ * @param {Number} digitCount
+ * @returns {Number}
+ */
+var getMaxProductWithGivenDigit = (digitCount) => {
+    let maxProduct = 0, tempProduct = 1;
+    const constantNum = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+    for(let i = 0;i < constantNum.length - 1 - digitCount; i++) {
+        tempProduct = 1;
+        for(let j = 0;j < digitCount; j++) {
+            tempProduct *= Number(constantNum.charAt(i + j));
+        }
+        if(tempProduct > maxProduct) {
+            maxProduct = tempProduct;
+        }
+    }
+    return maxProduct;
+};
+
+/**
+ * 
+ * @param {Number} edgeTotal
+ * @returns {Number} 
+ */
+var findSpecialPythagoreanTriplet = (edgeTotal) => {
+    /**
+     * we can use a general formula to find out all pythagorean triplet.
+     **     a = m^2 - n^2
+     **     b = 2 * m * n
+     **     c = m^2 + n^2
+     **     a + b + c = 2*m^2 + 2*m*n
+     */
+    let abcTotal = 0;
+    let isFound = false;
+    let m = 0 , n = 0;
+    let a, b, c;
+    for(let i = 1;i < 1000; i++) {
+        for(let j = 1;j <= i; j++) {
+            abcTotal = 2*i*i + 2*i*j;
+            if(abcTotal === edgeTotal) {
+                isFound = true;
+                m = i;
+                n = j;
+                break;
+            }
+        }
+        if(isFound) {
+            break;
+        }
+    }
+    a = m*m - n*n;
+    b = 2*m*n;
+    c = m*m + n*n;
+    return a*b*c;
+};
+
+/**
+ * 
+ * @param {Number} bound 
+ */
+var summationOfPrimes = (bound) => {
+    let total = 10;
+    for(let i = 3;i < bound; i+=2) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            continue;
+        }
+        if(isPrime(i)) {
+            total += i;
+        }
+    }
+    return total;
+};
